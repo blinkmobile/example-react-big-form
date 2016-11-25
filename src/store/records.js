8f0cdfb299387c12856e14b0e24d682a388de352
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-25 15:46:32 +1100

    load in random records, display list of records

--- src/store/records.js
+++ src/store/records.js
@@ -1,1 +1,28 @@
-export default new Map()
+import Chance from 'chance'
+
+const chance = new Chance()
+
+const randomRecordData = () => {
+  const record = {}
+  ;(new Array(200)).fill(null).forEach((unused, index) => {
+    const name = `field${index}`
+    if (index % 10 === 0) {
+      record[name] = (new Date(chance.timestamp())).toISOString().replace('Z', '')
+    } else if (index % 10 === 3) {
+      record[name] = chance.integer({ min: 0, max: 99 })
+    } else if (index % 10 === 5) {
+      record[name] = {}
+    } else {
+      record[name] = chance.sentence({ words: chance.integer({ min: 1, max: 4 }) })
+    }
+  })
+  return record
+}
+
+const records = new Map()
+
+chance.n(randomRecordData, 10).forEach((record) => {
+  records.set(chance.guid(), record)
+})
+
+export default records
--- src/views/ExistingRecords.js
+++ src/views/ExistingRecords.js
@@ -7,6 +7,12 @@ import './ExistingRecords.css'
 const ExistingRecords = () => (
   <div className='ExistingRecords'>
     <h1>ExistingRecords</h1>
+
+    <ul>
+      { Array.from(records.entries()).map(([ id, record ]) => (
+        <li key={id}>{id}</li>
+      )) }
+    </ul>
   </div>
 )
 
