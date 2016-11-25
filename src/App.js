2423c4b85f0cdf3916f11536519e33958a523c5a
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-25 17:01:03 +1100

    drop default content from `create-react-app`

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -10,10 +9,6 @@ class App extends Component {
     const isHome = path !== '/' && path !== '/home'
     return (
       <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-        </div>
         { !isHome && (
           <nav>
             <ul className='App-nav'>
