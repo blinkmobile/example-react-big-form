055c963f3f5949ab7b0a1ea8db1855ad56816f42
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-25 17:15:23 +1100

    populate and save values from SelectField

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,7 +38,7 @@ class Form extends Component {
             return <DateTimeField key={index} label={`Field ${index}`} name={name} onChange={this.handleFieldChange} value={value[name]} />
           }
           if (index % 10 === 3) {
-            return <SelectField key={index} label={`Field ${index}`} name={name} />
+            return <SelectField key={index} label={`Field ${index}`} name={name} onChange={this.handleFieldChange} value={value[name]} />
           }
           if (index % 10 === 5) {
             return <GeolocationField key={index} label={`Field ${index}`} name={name} />
--- src/components/SelectField.js
+++ src/components/SelectField.js
@@ -1,21 +1,56 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, PureComponent } from 'react'
 
 import './SelectField.css'
 
-const SelectField = ({ label }) => (
-  <fieldset className='SelectField'>
-    <label className='SelectField-label'>{label}</label>
-    <select className='SelectField-input'>
-      { (new Array(100)).fill(null).map((unused, index) => (
-        <option key={index} value={index}>{index}</option>
-      )) }
-    </select>
-  </fieldset>
-)
+class SelectField extends PureComponent {
+  constructor (props) {
+    super(props)
+
+    this.handleValueChange = this.handleValueChange.bind(this)
+
+    this.state = {
+      value: props.value
+    }
+  }
+
+  componentWillReceiveProps ({ value }) {
+    this.setState({ value })
+  }
+
+  handleValueChange (event) {
+    this.setState({ value: Number(event.target.value) }, () => {
+      this.notifyOnChange()
+    })
+  }
+
+  notifyOnChange () {
+    const { name, onChange } = this.props
+    if (onChange) {
+      onChange(name, this.state.value)
+    }
+  }
+
+  render () {
+    const { label } = this.props
+    const { value } = this.state
+    return (
+      <fieldset className='SelectField'>
+        <label className='SelectField-label'>{label}</label>
+        <select className='SelectField-input' onChange={this.handleValueChange} value={value}>
+          { (new Array(100)).fill(null).map((unused, index) => (
+            <option key={index} value={index}>{index}</option>
+          )) }
+        </select>
+      </fieldset>
+    )
+  }
+}
 
 SelectField.propTypes = {
   label: PropTypes.string,
-  name: PropTypes.string
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.number
 }
 
 export default SelectField
