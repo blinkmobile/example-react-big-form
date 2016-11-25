370d25d3c306b1738f8937053f9034bc1d795725
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-25 13:01:19 +1100

    add /home route and Home view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
@@ -10,12 +10,14 @@ class App extends Component {
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.props.children}
       </div>
     )
   }
+}
+
+App.propTypes = {
+  children: PropTypes.node
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { IndexRedirect, Router, Route, hashHistory } from 'react-router'
 
 import App from './App.js'
+import Home from './views/Home.js'
 
 import './index.css'
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path='/' component={App} />
+    <Route path='/' component={App}>
+      <IndexRedirect to='/home' />
+      <Route path='home' component={Home} />
+    </Route>
   </Router>,
   document.getElementById('root')
 )
